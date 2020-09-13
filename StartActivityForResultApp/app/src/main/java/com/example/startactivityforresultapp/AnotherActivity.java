bbf29bbc32eaf8f5dc1d53ecfe14a0b1a54d6965
ymm-ylx <2904916778@qq.com>
2020-09-13 22:57:33 +0800

    logs

---
+++ StartActivityForResultApp/app/src/main/java/com/example/startactivityforresultapp/AnotherActivity.java
@@ -0,0 +1,37 @@
+package com.example.startactivityforresultapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age", 20);
+        Toast.makeText(this,name+age,Toast.LENGTH_LONG).show();
+
+        Button button=(Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=getIntent();
+                String name=intent.getStringExtra("name");
+                Integer age=intent.getIntExtra("age", 20);
+                intent.putExtra("result","姓名:"+name+" 年龄"+age);
+                setResult(0,intent);
+                finish();
+            }
+        });
+    }
+
+}
---
+++ StartActivityForResultApp/app/src/main/java/com/example/startactivityforresultapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.startactivityforresultapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override  //完全生命周期开始时被调用，初始化Activity
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,AnotherActivity.class);
+                intent.putExtra("name","Zhangsan");
+                intent.putExtra("age",20);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0 && resultCode==0){
+            String str=data.getStringExtra("result");
+            Toast.makeText(this,str,Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+}
